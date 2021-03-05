95b56ddca83d26459d0836b429604799c4566132
mdraf <mdraf@192.168.1.128>
2021-03-05 15:18:32 +0300

    first commit

---
+++ RafiTestNGProject/src/test/java/webPortal/GoogleTest.java
@@ -0,0 +1,47 @@
+package webPortal;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+	
+    private WebDriver driver;
+  
+
+    @BeforeClass
+    public static void setupClass() {
+    	WebDriverManager.chromedriver().setup();
+	}
+    
+    @BeforeMethod
+    public void setupTest() {
+    	driver = new ChromeDriver();
+
+    	
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    	if(driver !=null) {
+    		driver.quit();
+    	}
+   	
+    }
+    
+    @Test
+    public void test1() throws InterruptedException {
+    	driver.get("https://www.google.com");
+    	driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[2]/div[1]/div[1]/div[1]/div[2]/input[1]")).sendKeys("Software Testing");
+    	driver.manage().window().maximize();
+    	Thread.sleep(2000);
+
+    	
+    }
+}
